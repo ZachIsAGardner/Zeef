8d7575f00a5d90621449bc37d091a563cae3dd97
Zachary Gardner <zachisagardner@gmail.com>
2018-08-29 23:20:38 -0400

    Update Sound example

---
+++ TwoDimensional/Scripts/Utilities/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	[SerializeField] Vector3 eulers;
+
+	void Update () {
+		transform.Rotate(eulers * Time.deltaTime);		
+	}
+}
