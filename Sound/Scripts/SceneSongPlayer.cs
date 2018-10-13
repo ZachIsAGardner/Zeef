9613bbb749e7241a5e99a325bca64b159fef987a
Zachary Gardner <zachisagardner@gmail.com>
2018-10-13 03:50:31 -0400

    Update SongPlayer.cs

--- Sound/Scripts/SceneSongPlayer.cs
+++ Sound/Scripts/SceneSongPlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Zeef.Sound {
@@ -9,7 +10,8 @@ namespace Zeef.Sound {
 
         protected virtual void Start() {
             if (songScriptable) AudioManager.ChangeSong(songScriptable);
-            else AudioManager.ChangeSong(AudioContent.GetSong(songScriptableName));
+            else if (!String.IsNullOrEmpty(songScriptableName)) AudioManager.ChangeSong(AudioContent.GetSong(songScriptableName));
+            else AudioManager.ChangeSong(null);
         }
     }
 }
