42c962fb424a068cfb902faa886d9cb5795a2306
Zachary Gardner <zachisagardner@gmail.com>
2019-06-27 21:37:13 -0400

    Light reorganizing. Add TargetTransformUI to Zeef.Menu

---
+++ General/Scripts/Misc/TimedDestruction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zeef 
+{
+    public class TimedDestruction : MonoBehaviour
+    {
+        public float lifeTime = 1;
+        private float startLifeTime;
+
+        void Awake() 
+        {
+            startLifeTime = lifeTime;    
+        }
+
+        void Update()
+        {
+            lifeTime -= 1 * Time.deltaTime;
+
+            if (lifeTime <= 0) 
+                Destroy(gameObject);        
+        }
+
+        public void Reset() 
+        {
+            if (startLifeTime <= 0) return; 
+
+            lifeTime = startLifeTime;
+        }
+    }
+}
---
+++ Menu/Scripts/TargetTransformUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zeef.GameManagement;
+
+namespace Zeef.Menu 
+{
+    public class TargetTransformUI : MonoBehaviour 
+    {
+        [SerializeField] private Transform target;
+
+        public static TargetTransformUI Initialize(TargetTransformUI prefab, Transform target) 
+        {
+			TargetTransformUI instance = GameManager.SpawnCanvasElement
+                (prefab.gameObject, Camera.main.WorldToScreenPoint(target.position)).GetComponent<TargetTransformUI>();
+
+			instance.target = target;
+
+			return instance;
+		}
+
+        void LateUpdate() 
+        {
+            if (target == null) return;
+            GetComponent<RectTransform>().position = Camera.main.WorldToScreenPoint(target.position);
+        }
+    }
+}
