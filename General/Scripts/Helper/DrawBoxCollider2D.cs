045aea4b69bba3ca86b7a289baf79a3a192f2f82
Zachary Gardner <zachisagardner@gmail.com>
2020-01-30 23:06:49 -0500

    Update draw box collider, add grid.cs

--- General/Scripts/Helper/DrawBoxCollider2D.cs
+++ General/Scripts/Helper/DrawBoxCollider2D.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Zeef {
-
-	public class DrawBoxCollider2D : MonoBehaviour {
-
+namespace Zeef 
+{
+	public class DrawBoxCollider2D : MonoBehaviour 
+	{
 		public Color Color = new Color(1, 1, 1, 0.5f);
 
-		void OnDrawGizmos() {
+		void OnDrawGizmos() 
+		{
 			Vector3 size = Vector3.one;
 			Vector3 offset = Vector3.zero;
 			BoxCollider2D box = GetComponent<BoxCollider2D>();
 
-			if (box != null) {
+			if (box != null) 
+			{
 				size = box.bounds.size;
 				offset = box.offset;
 			}
@@ -22,5 +24,4 @@ namespace Zeef {
 			Gizmos.DrawCube(transform.position + offset, size);
 		}
 	}
-
 }
---
+++ General/Scripts/Helper/Grid.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Zanitacharin
+{
+    public class Grid : MonoBehaviour
+    {
+        public Color Color = new Color(1, 1, 1, 0.5f);
+        public float Size = 1.28f;
+        public Vector2 Amount = new Vector2(10, 10);
+
+        void OnDrawGizmos() 
+        {
+            Vector3 size = Vector3.one * Size;
+            Vector3 offset = Vector3.one * Size * Amount / 2;
+            
+            Gizmos.color = Color;
+            
+            int i = 0;
+            while(i < Amount.x)
+            {
+                int j = 0;
+
+                while(j < Amount.y)
+                {
+                    if ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0))
+                    {
+                        Gizmos.DrawCube(
+                            transform.position + new Vector3(i * Size, j * Size, 0) - offset, 
+                            size
+                        );
+                    }
+
+                    j++;
+                }
+
+                i++;
+            }
+        }
+    }
+}
+
